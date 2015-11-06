bea79129de52837d19e437bdf796562e6a062b9b
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-11-06 16:24:16 +0300

    Update user_kp

--- user_kp/main.c
+++ user_kp/main.c
@@ -7,6 +7,10 @@
 #include "ontology.h"
 #include "common.h"
 
+static const double TEST_RADIUS = 1000;
+static const double TEST_LAT = 61.78;
+static const double TEST_LONG = 34.35;
+
 static volatile bool cont = true;
 
 static void signal_handler(int sig) {
@@ -19,10 +23,14 @@ static void subscribe_signals() {
     signal(SIGTERM, &signal_handler);
 }
 
-static void update_user_location(sslog_node_t* node, sslog_individual_t* user_individual, double lat, double lon) {
-    sslog_individual_t* new_location_individual = sslog_new_individual(CLASS_POINT, rand_uuid("user_point"));
-    sslog_insert_property(new_location_individual, PROPERTY_LATITUDE, double_to_string(lat));
-    sslog_insert_property(new_location_individual, PROPERTY_LONGITUDE, double_to_string(lon));
+static sslog_individual_t* update_user_location(sslog_node_t* node, sslog_individual_t* user_individual, double lat, double lon) {
+    sslog_individual_t* new_location_individual = sslog_new_individual(CLASS_LOCATION, rand_uuid("user_location"));
+    sslog_individual_t* new_point_individual = sslog_new_individual(CLASS_POINT, rand_uuid("user_point"));
+
+    sslog_insert_property(new_point_individual, PROPERTY_LAT, double_to_string(lat));
+    sslog_insert_property(new_point_individual, PROPERTY_LONG, double_to_string(lon));
+
+    sslog_node_insert_individual(node, new_point_individual);
     sslog_node_insert_individual(node, new_location_individual);
 
     const void* existing_location = sslog_get_property(user_individual, PROPERTY_HASLOCATION);
@@ -33,11 +41,17 @@ static void update_user_location(sslog_node_t* node, sslog_individual_t* user_in
     }
 
     sslog_node_update_property(node, user_individual, PROPERTY_HASLOCATION, (void*) existing_location, new_location_individual);
+    return new_location_individual;
+    // TODO: existing search request still uses old location
 }
 
-static sslog_individual_t* publish_search_request(sslog_node_t* node, sslog_individual_t* user_individual) {
+static sslog_individual_t* publish_search_request(sslog_node_t* node, sslog_individual_t* location_individual) {
+    sslog_individual_t* region_individual = sslog_new_individual(CLASS_CIRCLEREGION, rand_uuid("circle_search_region"));
+    sslog_insert_property(region_individual, PROPERTY_RADIUS, double_to_string(TEST_RADIUS));
+
     sslog_individual_t* request_individual = sslog_new_individual(CLASS_SEARCHREQUEST, rand_uuid("search_request"));
-    sslog_insert_property(request_individual, PROPERTY_CREATEDBY, user_individual);
+    sslog_insert_property(request_individual, PROPERTY_USELOCATION, location_individual);
+
     sslog_node_insert_individual(node, request_individual);
     return request_individual;
 }
@@ -54,14 +68,14 @@ static bool process_subscription_result(sslog_node_t* node, sslog_individual_t* 
         sslog_individual_t* point_individual = (sslog_individual_t*) entry->data;
         sslog_node_populate(node, point_individual);
 
-        double lat = parse_double(sslog_get_property(point_individual, PROPERTY_LATITUDE));
-        double lon = parse_double(sslog_get_property(point_individual, PROPERTY_LONGITUDE));
+        double lat = parse_double(sslog_get_property(point_individual, PROPERTY_LAT));
+        double lon = parse_double(sslog_get_property(point_individual, PROPERTY_LONG));
 
         printf("%d: Found point %lf %lf\n", counter, lat, lon);
 
         sslog_individual_t* point2 = sslog_new_individual(CLASS_POINT, rand_uuid("route_point"));
-        sslog_insert_property(point2, PROPERTY_LATITUDE, double_to_string(lat));
-        sslog_insert_property(point2, PROPERTY_LONGITUDE, double_to_string(lon));
+        sslog_insert_property(point2, PROPERTY_LAT, double_to_string(lat));
+        sslog_insert_property(point2, PROPERTY_LONG, double_to_string(lon));
 
         sslog_node_insert_individual(node, point2);
 
@@ -79,7 +93,7 @@ static bool process_subscription_result(sslog_node_t* node, sslog_individual_t* 
 
 static void subscribe_response(sslog_node_t* node, sslog_individual_t* request_individual) {
     list_t* properties = list_new();
-    list_add_data(properties, PROPERTY_PROCESSEDAT);
+    list_add_data(properties, PROPERTY_RESULTSIN);
 
     sslog_subscription_t* subscription = sslog_new_subscription(node, false);
     sslog_sbcr_add_individual(subscription, request_individual, properties);
@@ -120,9 +134,9 @@ int main() {
     sslog_node_insert_individual(node, user_individual);
 
     // Update user location
-    update_user_location(node, user_individual, 61.78, 34.35);
+    sslog_individual_t* location_individual = update_user_location(node, user_individual, TEST_LAT, TEST_LONG);
 
-    sslog_individual_t* request_individual = publish_search_request(node, user_individual);
+    sslog_individual_t* request_individual = publish_search_request(node, location_individual);
     subscribe_response(node, request_individual);
 
     sslog_node_remove_individual(node, request_individual);
