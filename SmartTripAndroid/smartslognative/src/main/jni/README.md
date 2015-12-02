c493cd4e0fa3ee661e0826409208495acb6bafa2
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-12-02 15:21:20 +0300

    Add native third-party sources

---
+++ SmartTripAndroid/smartslognative/src/main/jni/README.md
@@ -0,0 +1,4 @@
+* ckpi v0.31alpha with "nota" files deleted
+* expat 2.1.0
+* scew 1.1.7
+* smartslog_dapi
--- SmartTripAndroid/smartslognative/src/main/jni/smartlognative.c
+++ SmartTripAndroid/smartslognative/src/main/jni/smartlognative.c
@@ -10,3 +10,60 @@ JNIEXPORT jint JNICALL
 Java_org_fruct_oss_tsp_smartslognative_NativeTest_divide(JNIEnv *env, jclass type, jint a, jint b) {
     return a / b;
 }
+
+JNIEXPORT void JNICALL
+Java_org_fruct_oss_tsp_smartslognative_JniSmartSpaceNative_initialize(JNIEnv *env, jobject instance,
+                                                                      jstring userId_) {
+    const char *userId = (*env)->GetStringUTFChars(env, userId_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, userId_, userId);
+}
+
+JNIEXPORT void JNICALL
+Java_org_fruct_oss_tsp_smartslognative_JniSmartSpaceNative_shutdown(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+}
+
+JNIEXPORT void JNICALL
+Java_org_fruct_oss_tsp_smartslognative_JniSmartSpaceNative_updateUserLocation(JNIEnv *env,
+                                                                              jobject instance,
+                                                                              jdouble lat,
+                                                                              jdouble lon) {
+
+    // TODO
+
+}
+
+JNIEXPORT void JNICALL
+Java_org_fruct_oss_tsp_smartslognative_JniSmartSpaceNative_postSearchRequest(JNIEnv *env,
+                                                                             jobject instance,
+                                                                             jdouble radius,
+                                                                             jstring pattern_) {
+    const char *pattern = (*env)->GetStringUTFChars(env, pattern_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, pattern_, pattern);
+}
+
+JNIEXPORT void JNICALL
+Java_org_fruct_oss_tsp_smartslognative_JniSmartSpaceNative_postScheduleRequest(JNIEnv *env,
+                                                                               jobject instance,
+                                                                               jobjectArray points) {
+
+    // TODO
+
+}
+
+JNIEXPORT void JNICALL
+Java_org_fruct_oss_tsp_smartslognative_JniSmartSpaceNative_setListener(JNIEnv *env,
+                                                                       jobject instance,
+                                                                       jobject listener) {
+
+    // TODO
+
+}
